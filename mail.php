d6b09f26728e4ccbf6c6a83d3dac81a96837d201
gui-farias <gui.farias@outlook.com.br>
2020-09-06 21:55:24 -0300

    subindo projeto

--- mail.php
+++ mail.php
@@ -18,7 +18,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "admin@devitems.com";
+        $recipient = "guilherme@elaroye.com.br";
 
         // Set the email subject.
         $subject = "New contact from $name";
